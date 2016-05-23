868b7e6ee25b4a7a4971744da05e08cf60288b0e
LOZORD <ljrudberg@gmail.com>
2016-05-23 18:25:20 -0500

    Chapter 3 modifications

--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"github.com/russross/blackfriday"
 	"net/http"
 )
 
 func main() {
 	location := ":8000"
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
 	fmt.Println("Visit `localhost" + location + "`")
-	http.ListenAndServe(location, http.FileServer(http.Dir(".")))
+	http.ListenAndServe(location, nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	formData := []byte(r.FormValue("body"))
+	markdown := blackfriday.MarkdownCommon(formData)
+	rw.Write(markdown)
 }
