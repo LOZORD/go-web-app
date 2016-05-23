28ff8cbf46d2d166c01b75c5034a58cf5b3a1e6a
LOZORD <ljrudberg@gmail.com>
2016-05-23 18:09:24 -0500

    Added Makefile

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir(".")))
+	location := ":8000"
+	fmt.Println("Visit `localhost" + location + "`")
+	http.ListenAndServe(location, http.FileServer(http.Dir(".")))
 }
