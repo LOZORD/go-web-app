1907c339a2931b01040cbe0f266c3b676e9ef498
LOZORD <ljrudberg@gmail.com>
2016-06-01 16:59:34 -0500

    Added 'AddBook' function

--- databases/main.go
+++ databases/main.go
@@ -18,6 +18,8 @@ func main() {
 	db := NewDB()
 	loc := ":8080"
 	log.Println("Listening on " + loc)
+	bookToAdd := Book{"The Giving Tree", "Shel Silverstein"}
+	AddBook(&bookToAdd, db)
 	http.ListenAndServe(loc, ShowBooks(db))
 }
 
@@ -52,9 +54,25 @@ func NewDB() *sql.DB {
 	return db
 }
 
-/* TODO
-func addBook(book *Book, db *sql.DB) {
+func AddBook(book *Book, db *sql.DB) bool {
 	dupQ := "select (count(*) > 0) from books where title=? and author=?"
-	err := db.QueryRow(dupQ, book.title, book.author)
+	var dupExists bool
+	dupError := db.QueryRow(dupQ, book.Title, book.Author).Scan(&dupExists)
+
+	if dupError != nil {
+		panic(dupError)
+	} else if dupExists {
+		return false
+	} else {
+		// we are ok to insert a new entry
+	}
+
+	insQ := "insert into books (title, author) values (?, ?)"
+	_, insertError := db.Exec(insQ, book.Title, book.Author)
+
+	if insertError != nil {
+		panic(insertError)
+	} else {
+		return true
+	}
 }
-*/
